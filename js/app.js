6c45d7d6937308fbac23e7d2f3865cb5fc232c30
AmirAli Mousa <amiralimomo77@gmail.com>
2021-02-20 13:40:51 +0330

    btn by course

--- js/app.js
+++ js/app.js
@@ -1,24 +1,21 @@
 // variables
 const courses = document.querySelector('#courses-list')
 
-
 // eventListeners
 eventListeners()
-function eventListeners(){
+function eventListeners() {
     courses.addEventListener('click', buyCourse)
 }
 
-
 // functions
-function buyCourse(e){
+function buyCourse(e) {
     e.preventDefault()
     if(e.target.classList.contains('add-to-cart')){
-        const course = e.target.parentElement.parentElement
+        const course=e.target.parentElement.parentElement
         getCourseInfo(course)
+      
     }
 }
-
 function getCourseInfo(course){
-    console.log(course)
-}
 
+}
